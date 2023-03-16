7583003b07fe026466527d97f6fdb35befbb5465
Matthew Thornton <MatthewThornton.USA@gmail.com>
2023-03-16 13:02:01 -0700

    add axios and semantic ui

--- API/Program.cs
+++ API/Program.cs
@@ -3,13 +3,17 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Persistence.DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
 });
 
 
@@ -21,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
  
---
+++ client-app/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Header, ListItem, List } from "semantic-ui-react";
+
+function App() {
+  const [activities, setActivities] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get("http://localhost:4999/api/activities").then((response) => {
+      console.log(response);
+      setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Header as="h2" icon="users" content="Reactivites" />
+      <List>
+        {activities.map((activity: any) => (
+          <ListItem key={activity.id}>{activity.title}</ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default App;
