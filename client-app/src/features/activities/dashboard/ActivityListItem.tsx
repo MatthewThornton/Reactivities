b613a96a9a15ad0de3de387fab4e1d8286d35884
Matthew Thornton <MatthewThornton.USA@gmail.com>
2023-04-27 15:15:22 -0700

    finish with chapter 9

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,28 +1,13 @@
-import React, { SyntheticEvent, useState } from "react";
-import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
+import React from "react";
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activitiy";
 import { Link } from "react-router-dom";
-import { useStore } from "../../../app/stores/store";
-
 interface Props {
   activity: Activity;
 }
 
 const ActivityListItem = ({ activity }: Props) => {
-  const { activityStore } = useStore();
-  const { deleteActivity, loading } = activityStore;
-
-  const [target, setTarget] = useState("");
-
-  function handleDeleteActivity(
-    e: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
-  console.log("activity", activity);
-
+ 
   return (
     <>
       <Segment.Group>
